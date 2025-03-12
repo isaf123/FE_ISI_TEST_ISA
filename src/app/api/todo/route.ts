0a4643a5e3f9d96f0d356447172401be6a78aaf1
Isa <137991239+isaf123@users.noreply.github.com>
2025-03-12 13:52:17 +0700

    feat: patch to-do-list

--- src/app/api/todo/route.ts
+++ src/app/api/todo/route.ts
@@ -2,6 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import { verifyToken } from "@/lib/verifyToken";
 import ApiError from "@/types/error.types";
 import prisma from "@/lib/prisma";
+import logger from "@/lib/logger";
 
 export async function POST(req: Request) {
   try {
@@ -9,36 +10,76 @@ export async function POST(req: Request) {
     if (userToken.role != "Lead")
       throw { message: "unaothorized", status: 401 };
     const { title, description, pic_id } = await req.json();
-    const createTodo = await prisma.toDo.create({
+    const todo = await prisma.toDo.create({
       data: {
         title,
         description,
         pic_id,
         createdby_id: userToken.id,
         status: "not_started",
+        Log: { create: { updateby_id: userToken.id, action: "create" } },
       },
     });
+    logger.info(`create todo success`);
     return NextResponse.json({
       message: "to do list created",
       status: 200,
-      data: createTodo,
+      data: todo,
     });
   } catch (error) {
     const err = error as ApiError;
+    logger.error({ message: err.message, status: err.status });
     return NextResponse.json(
       { message: err.message, status: err.status },
       { status: err.status }
     );
   }
 }
+
 export async function GET(req: NextRequest) {
   const userToken = verifyToken(req);
-  const id = req.nextUrl.searchParams.get("pic_id");
   if (!userToken) throw { message: "unaothorized", status: 401 };
+
+  const id = req.nextUrl.searchParams.get("pic_id");
   const findTodo = await prisma.toDo.findMany({
     where: id ? { pic_id: Number(id) } : {},
   });
   return NextResponse.json(findTodo);
 }
 
-export async function UPDATE() {}
+export async function PATCH(req: NextRequest) {
+  try {
+    const userToken = verifyToken(req);
+
+    if (!userToken) throw { message: "unaothorized", status: 401 };
+    const { desc, todo_id, status_to } = await req.json();
+    const findTodo = await prisma.toDo.findFirst({ where: { todo_id } });
+    if (!findTodo) throw { message: "todo not exist", status: 400 };
+
+    const update = await prisma.toDo.update({
+      where: { todo_id },
+      data: {
+        description: desc ? desc : findTodo.description,
+        status: status_to ? status_to : findTodo.status,
+        Log: {
+          create: {
+            updateby_id: userToken.id,
+            action: "update",
+            updatedesc: desc ? desc : null,
+            status_from: status_to ? findTodo.status : null,
+            status_to: status_to ? status_to : null,
+          },
+        },
+      },
+    });
+    logger.info(`update todo success (id:${update.todo_id})`);
+    return NextResponse.json({ message: "update success", data: update });
+  } catch (error) {
+    const err = error as ApiError;
+    logger.error({ message: err.message, status: err.status });
+    return NextResponse.json(
+      { message: err.message, status: err.status },
+      { status: err.status }
+    );
+  }
+}
